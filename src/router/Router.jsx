094912989c8d0ce8ba2895e7b1ada1bb79d756dc
Kohei-Takano <132127471+Kohei-Takano@users.noreply.github.com>
2023-05-14 21:24:05 +0900

    add route last

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,5 +1,6 @@
 import { Switch, Route } from "react-router-dom";
 import { Home } from "../Home";
+import { Page404 } from "../Page404";
 import { Page1Routes } from "./Page1Routes";
 import { Page2Routes } from "./Page2Routes";
 export const Router = () => {
@@ -40,6 +41,9 @@ export const Router = () => {
           </Switch>
         )}
       />
+      <Route path="*">
+        <Page404 />
+      </Route>
     </Switch>
   );
 };
